e324b9ca0f52e26b8e3ac80d3851e8a856bdef2e
Trev <daeteri360@gmail.com>
2021-11-04 13:49:16 -0400

    collision update

--- GAME208_AR/Assets/Scripts/CollisionDetection.cs
+++ GAME208_AR/Assets/Scripts/CollisionDetection.cs
@@ -2,20 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CollsionDetect : MonoBehaviour
+public class CollisionDetection : MonoBehaviour
 {
     public GameController GameControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnCollisionEnter(Collision collision)

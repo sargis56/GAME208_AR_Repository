06f5301c81a3d1f1f715f4b5d3693e181085a9ae
Trev <daeteri360@gmail.com>
2021-11-06 18:45:38 -0400

    AI for 2x1 and 3x1 boats done

--- GAME208_AR/Assets/Scripts/AIController.cs
+++ GAME208_AR/Assets/Scripts/AIController.cs
@@ -40,27 +40,30 @@ public class AIController : MonoBehaviour
         int randX, randZ;
         //boat 1
         randX = Random.Range(0, 10); //generate random pos for boat 1
-        randZ = Random.Range(1, 8);
-        if (AIPlacementArray[randX, randZ] == false && AIPlacementArray[randX - 1, randZ] == false)
+        randZ = Random.Range(1, 9);
+        Debug.Log("X " + randX + " Z " + randZ);
+        if (AIPlacementArray[randX, randZ] == false || AIPlacementArray[randX, randZ - 1] == false)
         {
             boatCollision1.transform.position = new Vector3(boatCollision1.transform.position.x + (randX * -spaceSize), boatCollision1.transform.position.y - (randZ * spaceSize), boatCollision1.transform.position.z);
             AIPlacementArray[randX, randZ] = true;
             AIPlacementArray[randX, randZ - 1] = true;
         }
         //boat 2
-        randX = Random.Range(0, 6); //generate random pos for boat 2
-        randZ = Random.Range(0, 10);
-        if (AIPlacementArray[randX, randZ] == false && AIPlacementArray[randX + 1, randZ] == false && AIPlacementArray[randX + 2, randZ] == false)
+        randX = Random.Range(1, 8); //generate random pos for boat 2
+        randZ = Random.Range(1, 10);
+        Debug.Log("X " + randX + " Z " + randZ);
+        if (AIPlacementArray[randX, randZ] == false || AIPlacementArray[randX - 1, randZ] == false || AIPlacementArray[randX - 2, randZ] == false)
         {
             boatCollision2.transform.position = new Vector3(boatCollision2.transform.position.x + (randX * -spaceSize), boatCollision2.transform.position.y - (randZ * spaceSize), boatCollision2.transform.position.z);
             AIPlacementArray[randX, randZ] = true;
-            AIPlacementArray[randX + 1, randZ] = true;
-            AIPlacementArray[randX + 2, randZ] = true;
+            AIPlacementArray[randX - 1, randZ] = true;
+            AIPlacementArray[randX - 2, randZ] = true;
         }
         //boat 3
         randX = Random.Range(0, 10); //generate random pos for boat 3
-        randZ = Random.Range(2, 7);
-        if (AIPlacementArray[randX, randZ] == false && AIPlacementArray[randX - 1, randZ] == false && AIPlacementArray[randX - 2, randZ] == false)
+        randZ = Random.Range(1, 8);
+        Debug.Log("X " + randX + " Z " + randZ);
+        if (AIPlacementArray[randX, randZ] == false || AIPlacementArray[randX, randZ - 1] == false || AIPlacementArray[randX, randZ - 2] == false)
         {
             boatCollision3.transform.position = new Vector3(boatCollision3.transform.position.x + (randX * -spaceSize), boatCollision3.transform.position.y - (randZ * spaceSize), boatCollision3.transform.position.z);
             AIPlacementArray[randX, randZ] = true;
